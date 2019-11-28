4f1b26e6146492de83a862637fd8c18220245e03
Augusto Moço Zambello <gutomz@outlook.com>
2019-11-28 00:20:22 -0300

    v2.0
    Nova versão estável.

--- interfaceCrypto.c
+++ interfaceCrypto.c
@@ -5,93 +5,202 @@
 #include <unistd.h>
 #include <string.h>
 
+int openFile(int operation){
+	int fdCryptoFile;
+
+	switch(operation){
+		case 1:
+		default:
+			fdCryptoFile = open("/home/guto/Desktop/cryptoFile.txt", O_RDONLY, 0666);
+		break;
+
+		case 2:
+			fdCryptoFile = open("/home/guto/Desktop/cryptoFile.txt", O_RDWR | O_CREAT | O_APPEND, 0666);
+		break;
+	}
+
+	if (fdCryptoFile == -1)
+	{
+		printf("Erro ao abrir o arquivo\n");
+		getchar();
+		return -1;
+	}
+
+	return fdCryptoFile;
+}
+
 
 int main()
 {
-        char cryptoWord[256];
-	char decryptoResult[256];
-
-	int fdCryptoFile;
 	struct stat st;
 
-	int opcao;
+	int opcao, i, ret, status;
+	int fileSize;
+	int fdCryptoFile;
+	int cryptoWordSize;
+	char *msg;
+	char *oldMsg;
+	char c;
 
-	size_t cryptoWordSize;
+	do{
+		do{
+			system("clear");
+			printf("Escolha o que deseja fazer:\n");
+			printf("1 - Ler o arquivo (Criptografado)\n");
+			printf("2 - Ler o arquivo (Descriptografado)\n");
+			printf("3 - Escrever no arquivo\n");
+			printf("4 - Apagar o arquivo\n");
+			printf("0 - Sair\n");
+			printf("Resposta: ");
+			scanf("%d", &opcao);
+		}while(opcao < 0 || opcao > 4);
+		getchar();
 
-        do
-        {
-		system("clear");
-                printf("Escolha o que deseja fazer:\n1-Cryptografar uma Palavra\n2-Descriptografar o arquivo\n3-Sair\n");
-                scanf("%d", &opcao);
+		switch (opcao)
+		{
+			case 1:
+				
+				if((fdCryptoFile = openFile(1)) == -1) break;
 
-                switch (opcao)
-                {
-                case 1:
-			fdCryptoFile = open("/home/guto/Desktop/cryptoFile.txt", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH, 0666);
-			if (fdCryptoFile == -1)
-			{
-					printf("Erro ao abrir o CryptoFile\n");
+				stat("/home/guto/Desktop/cryptoFile.txt", &st);
+				fileSize = st.st_size;
+
+				if(fileSize <= 0){
+					printf("Arquivo vazio!\n");
+					getchar();
 					break;
-			}		
-	
-                        printf("Digite a frase a ser criptografada:\n");
-			getchar();
-                        scanf("%[^\n]", cryptoWord);
-			cryptoWordSize = strlen(cryptoWord);
-			cryptoWord[(int)cryptoWordSize] = '\0';
+				}
 
+				msg = malloc(fileSize);
 
-			printf("%s\n", cryptoWord);
-                        //fazer syscall writeCrypto
-			
-			if(syscall(548, fdCryptoFile, cryptoWord, cryptoWordSize)){
-				printf("Mensagem foi criptografada corretamente\n");
-			}else{
-				printf("Algo deu errado ...\n");
-			}
+				ret = read(fdCryptoFile, msg, fileSize);
 
-			close(fdCryptoFile);
-  			
-			getchar();
-                        break;
+				if(ret == -1){
+					printf("Não foi possível ler o arquivo!\n");
+					if(msg) free(msg);
+					getchar();
+					break;
+				}
 
-                case 2:
-			getchar();
-			fdCryptoFile = open("/home/guto/Desktop/cryptoFile.txt", O_RDONLY, 0666);
-			if (fdCryptoFile == -1)
-			{
-				printf("Erro ao abrir o CryptoFile\n");
+				system("clear");
+				for(i = 0; i < ret; i++) printf("%c", msg[i]);
+				printf("\n\nPressione qualquer tecla para continuar ...");
+
+				if(msg) free(msg);
+				close(fdCryptoFile);
 				getchar();
 
-				break;
-			}
+			break;
 
-			stat("/home/guto/Desktop/cryptoFile.txt", &st);
-			cryptoWordSize = st.st_size;
+			case 2:
+				
+				if((fdCryptoFile = openFile(1)) == -1) break;
 
-			if(cryptoWordSize > 0){
-			
-		                printf("Frase criptografada:\n");
+				stat("/home/guto/Desktop/cryptoFile.txt", &st);
+				fileSize = st.st_size;
 
-				if(syscall(549, fdCryptoFile, decryptoResult, cryptoWordSize) == -1){
-					printf("Algo deu errado ...\n");
+				if(fileSize <= 0){
+					printf("Arquivo vazio!\n");
+					getchar();
+					break;
 				}
-	  			else printf("%s\n", decryptoResult);
-			}else{
-				printf("O arquivo está vazio!\n");
-			}
 
-			getchar();
+				msg = malloc(fileSize);
 
-			close(fdCryptoFile);
-                        break;
+				ret = syscall(549, fdCryptoFile, msg, fileSize);
 
-                default:
-                        break;
-                }
+				if(ret == -1){
+					printf("Não foi possível ler o arquivo!\n");
+					if(msg) free(msg);
+					getchar();
+					break;
+				}
 
-        }while(opcao != 3);
+				system("clear");
+				for(i = 0; i < ret; i++) {
+					if(msg[i] != '\0')
+						printf("%c", msg[i]);
+				}
+				printf("\n\nPressione qualquer tecla para continuar ...");
 
-        return 0;
+				if(msg) free(msg);
+				close(fdCryptoFile);
+				getchar();
+			break;
+
+			case 3:
+
+				if((fdCryptoFile = openFile(2)) == -1) break;
+
+				stat("/home/guto/Desktop/cryptoFile.txt", &st);
+				fileSize = st.st_size;
+
+				if(fileSize > 0){
+					oldMsg = malloc(fileSize);
+					ret = syscall(549, fdCryptoFile, oldMsg, fileSize);
+
+					if(ret == -1){
+						printf("Erro ao carregar o arquivo! Tente novamente mais tarde ...");
+						if(oldMsg) free(oldMsg);
+						getchar();
+						break;
+					}
+
+				}
+
+				msg = malloc(500);
+
+				system("clear");
+				printf("Digite a mensagem (MAX 500 CARACTERES):\n\n");
+
+				if(oldMsg) 
+					for(i = 0; i < ret; i++) 
+						if(oldMsg[i] != '\0') printf("%c", oldMsg[i]);
+
+				scanf("%[^\n]", msg);
+
+				cryptoWordSize = strlen(msg);
+				if(cryptoWordSize > 500) {
+					cryptoWordSize = 500;
+					__fpurge(stdin);
+				}
+
+				ret = syscall(548, fdCryptoFile, msg, cryptoWordSize);
+
+				if(ret) printf("\nMensagem Criptografada corretamente!\n");
+				else printf("\nAlgo deu errado. Tente novamente!\n");
+				getchar();
+
+				if(msg) free(msg);
+				if(oldMsg) free(oldMsg);
+				close(fdCryptoFile);
+				getchar();
+
+			break;
+
+			case 4:
+				status = remove("/home/guto/Desktop/cryptoFile.txt");
+
+				system("clear");
+
+				if(!status) printf("Arquivo apagado com sucesso!\n");
+				else printf("Não foi possivel apagar o arquivo!\n");
+
+				getchar();
+			break;
+
+			case 0:
+			default:
+				opcao = 0;
+			break;
+
+		}
+
+		oldMsg = NULL;
+		msg = NULL;
+
+	}while(opcao != 0);
+
+    return 0;
 
 }
